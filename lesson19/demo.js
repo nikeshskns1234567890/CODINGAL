8e3f3452550082ad1d3e3ff3b4ed5baaf79f649d
nikeshskns1234567890 <nsivaraman2006@gmail.com>
2025-06-12 23:06:57 +0530

    activity commit

---
+++ lesson19/demo.js
@@ -0,0 +1,24 @@
+console.log("hi bros learning java script is so fun")
+// this is javascript
+
+var nikesh987 = 14
+var NIKESH987 = 45
+var nIkesh978 = 56
+var hitler = "I will conquer the entire world"
+var sungjinwoo = true
+var sungjinwoo2 = false
+
+console.log(nikesh987)
+console.log(NIKESH987)
+console.log(nIkesh978)
+
+var a = 400
+var b = 600
+var c = a+b
+var d = a-b
+var e = a/b
+var f = a%b
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
