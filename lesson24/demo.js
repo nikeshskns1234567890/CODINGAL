15792d71a49210db779dc10bfab6ccbef3f01753
nikeshskns1234567890 <nsivaraman2006@gmail.com>
2025-07-01 22:56:07 +0530

    nikesh js commit

---
+++ lesson24/demo.js
@@ -0,0 +1,17 @@
+document.getElementById('headingid').innerHTML = 'siuuuuuuuuuu'
+
+console.log(document.title)
+
+const e = document.getElementsByClassName('nizkingclass') 
+e[0].innerHTML = 'nikesh html got overwritten'
+
+const heading = document.getElementById("headingid");
+heading.style.backgroundColor = "lightcoral";
+
+const list = document.querySelector("ul");
+list.style.backgroundColor = "powderblue";
+
+const paragraph = document.querySelector(".nizkingclass");
+paragraph.style.backgroundColor = "honeydew";
+paragraph.style.border = "2px solid green";
+paragraph.style.padding = "10px";
