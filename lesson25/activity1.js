dee8b01c77ca616443cd5f73b8936ee4ba4bb615
nikeshskns1234567890 <nsivaraman2006@gmail.com>
2025-07-03 23:24:24 +0530

    nikesh js commit

---
+++ lesson25/activity1.js
@@ -0,0 +1,36 @@
+var a = "football is life"
+var b ="The pitch is my canvas"
+var c ="Legends never rest"
+
+const d = "FC barcelona"
+const e = "lionel messi"
+const f = "camp nou"
+
+let g = "midfilder"
+let h = "toni kroos"
+let i = "last minute goal"
+
+let j = "4.5"
+let k = "30"
+let l = "40"
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(j)
+console.log(k)
+console.log(l)
+
+{
+    var m ="ronaldo nazario"
+}
+console.log(m)
+
+{const x = "bergkamp"
+console.log(x)}
