dc4ff74bb517e08bcb95a96328fc5d02074bcf1c
nikeshskns1234567890 <nsivaraman2006@gmail.com>
2025-07-12 21:42:30 +0530

    done

---
+++ lesson25/project1.js
@@ -0,0 +1,38 @@
+var a = "badminton is passion";
+var b = "The court is my battlefield";
+var c = "Champions rise in silence";
+
+const d = "PV Sindhu";
+const e = "Lin Dan";
+const f = "All England Open";
+
+let g = "smash master";
+let h = "Tai Tzu-ying";
+let i = "rally of the century";
+
+let j = "21";     
+let k = "3";        
+let l = "2";        
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+
+{
+    var m = "Kidambi Srikanth";
+}
+console.log(m);
+
+{
+    const x = "Carolina Marín";
+    console.log(x);
+}
