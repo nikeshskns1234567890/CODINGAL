ea594bc1f137563fc7b5cfa4982046e0ee693448
nikeshskns1234567890 <nsivaraman2006@gmail.com>
2025-08-19 23:01:27 +0530

    dobe

---
+++ lesson38/demo.py
@@ -0,0 +1,37 @@
+print('python is finally working')# Python Operators and Data Types Example
+x = 56
+y = 'Soumil is at peace today '
+z = True
+a = False
+b = 62.67
+
+print(type(x))
+print(type(b))
+print(type(y))
+print(type(a))
+
+x = str(x)   # float(), int(), str(), bool()
+print(type(x))
+
+a = 48
+b = 7
+print(a/b)
+print(a//b)  # floor division
+
+print(9**9)
+
+print(10 <= 8)
+print(5 != 6)
+
+print(8/5+2-0+86-230%20**4/2*-2+2/a**b)
+
+print(len(y))
+print(y[3])
+print(y[3:11:1])
+print(y[3:11:2])
+
+print("ankush" + y)
+print(y * 4)
+
+ag = input("Hi what is your age?")
+print("the age of the user is :", ag)
